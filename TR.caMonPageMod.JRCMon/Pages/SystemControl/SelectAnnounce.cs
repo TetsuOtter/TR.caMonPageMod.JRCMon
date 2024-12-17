7682622a28f6f2e87b5ba6353ed1915b33cbdeea
TetsuOtter <31824852+TetsuOtter@users.noreply.github.com>
2024-12-18 00:22:47 +0900

    自動放送ページもページ送り可能に

--- TR.caMonPageMod.JRCMon/Pages/SystemControl/SelectAnnounce.cs
+++ TR.caMonPageMod.JRCMon/Pages/SystemControl/SelectAnnounce.cs
@@ -3,9 +3,11 @@ using TR.caMonPageMod.JRCMon.Footer;
 namespace TR.caMonPageMod.JRCMon.Pages.SystemControl;
 
 [PageTypes.FullScreenPage]
-public partial class SelectAnnounce : FullScreenPageBase, IFooterInfo
+public partial class SelectAnnounce : FullScreenPageBase, IMultiPageFooterInfo
 {
 	public IReadOnlyList<FooterInfo> FooterInfoList => FooterType.SELECT_ANNOUNCE;
+	public int SelectedIndex { get; set; }
+	public int MaxIndex { get; } = 2;
 
 	public SelectAnnounce() : base(ResourceManager.ResourceFiles.SelectAnnounce)
 	{
