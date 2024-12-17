96cda912fc57d895502636bf934f0c15de508e18
TetsuOtter <31824852+TetsuOtter@users.noreply.github.com>
2024-12-18 00:14:27 +0900

    検修系

--- TR.caMonPageMod.JRCMon/Pages/Maintenance/MaintenanceMenu.cs
+++ TR.caMonPageMod.JRCMon/Pages/Maintenance/MaintenanceMenu.cs
@@ -1,13 +1,29 @@
+using System.Windows;
+using System.Windows.Controls;
+
 using TR.caMonPageMod.JRCMon.Footer;
 
 namespace TR.caMonPageMod.JRCMon.Pages.Maintenance;
 
 [PageTypes.NormalPage("検修メニュー", ResourceManager.ResourceFiles.MaintenanceIcon)]
-public partial class MaintenanceMenuPage : NormalPageBase, IFooterInfo
+public partial class MaintenanceMenuPage : NormalPageBase, IHoldRootGridInstance, IFooterInfo
 {
+	public RootGrid? RootGrid { get; set; }
 	public IReadOnlyList<FooterInfo> FooterInfoList => FooterType.MAINTENANCE_MENU;
 
 	public MaintenanceMenuPage() : base(ResourceManager.ResourceFiles.MaintenanceMenu)
 	{
+		AddButtonWithXY<PerformanceRecordPage>(191, 230);
+		AddButtonWithXY<FrameworkElement>(508, 230, true);
+	}
+
+	void AddButtonWithXY<T>(int x, int y, bool isNotImplemented = false) where T: FrameworkElement, new()
+	{
+		Button btn = ComponentFactory.GetEmptyButton(new(x, y, 0, 0), 113, 67);
+		if (isNotImplemented)
+			btn.Content = "未実装";
+		else
+			btn.Click += (s, e) => RootGrid?.SetPageType<T>();
+		Children.Add(btn);
 	}
 }
--- TR.caMonPageMod.JRCMon/Pages/Maintenance/PerformanceRecord.cs
+++ TR.caMonPageMod.JRCMon/Pages/Maintenance/PerformanceRecord.cs
@@ -2,7 +2,7 @@ using TR.caMonPageMod.JRCMon.Footer;
 
 namespace TR.caMonPageMod.JRCMon.Pages.Maintenance;
 
-[PageTypes.NormalPage("検修メニュー", ResourceManager.ResourceFiles.MaintenanceIcon)]
+[PageTypes.NormalPage("性能記録", ResourceManager.ResourceFiles.MaintenanceIcon)]
 public partial class PerformanceRecordPage : NormalPageBase, IFooterInfo
 {
 	public IReadOnlyList<FooterInfo> FooterInfoList => FooterType.PERFORMANCE_RECORD;
