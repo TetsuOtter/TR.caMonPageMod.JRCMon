d4b0dc0ffd48794445be395c2001c37664ca6e6f
TetsuOtter <31824852+TetsuOtter@users.noreply.github.com>
2024-12-18 00:08:11 +0900

    補正系

--- TR.caMonPageMod.JRCMon/Pages/Correction/ClockCorrection.cs
+++ TR.caMonPageMod.JRCMon/Pages/Correction/ClockCorrection.cs
@@ -2,7 +2,7 @@ using TR.caMonPageMod.JRCMon.Footer;
 
 namespace TR.caMonPageMod.JRCMon.Pages.Correction;
 
-[PageTypes.NormalPage("時刻", ResourceManager.ResourceFiles.CorrectionIcon)]
+[PageTypes.NormalPage("時刻設定", ResourceManager.ResourceFiles.CorrectionIcon)]
 public partial class ClockCorrection : NormalPageBase, IFooterInfo
 {
 	public IReadOnlyList<FooterInfo> FooterInfoList => FooterType.CLOCK_CORRECTION;
--- TR.caMonPageMod.JRCMon/Pages/Correction/CorrectionMenu.cs
+++ TR.caMonPageMod.JRCMon/Pages/Correction/CorrectionMenu.cs
@@ -1,13 +1,29 @@
+using System.Windows;
+using System.Windows.Controls;
+
 using TR.caMonPageMod.JRCMon.Footer;
 
 namespace TR.caMonPageMod.JRCMon.Pages.Correction;
 
 [PageTypes.NormalPage("補正メニュ", ResourceManager.ResourceFiles.CorrectionIcon)]
-public partial class CorrectionMenu : NormalPageBase, IFooterInfo
+public partial class CorrectionMenu : NormalPageBase, IHoldRootGridInstance, IFooterInfo
 {
+	public RootGrid? RootGrid { get; set; }
 	public IReadOnlyList<FooterInfo> FooterInfoList => FooterType.CORRECTION_MENU;
 
 	public CorrectionMenu() : base(ResourceManager.ResourceFiles.CorrectionMenu)
 	{
+		AddButtonWithXY<ClockCorrection>(157, 230);
+		AddButtonWithXY<FrameworkElement>(532, 230, true);
+	}
+
+	void AddButtonWithXY<T>(int x, int y, bool isNotImplemented = false) where T: FrameworkElement, new()
+	{
+		Button btn = ComponentFactory.GetEmptyButton(new(x, y, 0, 0), 132, 68);
+		if (isNotImplemented)
+			btn.Content = "未実装";
+		else
+			btn.Click += (s, e) => RootGrid?.SetPageType<T>();
+		Children.Add(btn);
 	}
 }
