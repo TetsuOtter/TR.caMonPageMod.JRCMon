ebaf2f291bed7e378619f40b9446d74bbdb71446
TetsuOtter <31824852+TetsuOtter@users.noreply.github.com>
2024-12-28 17:20:07 +0900

    メニュー画面の描画を自前化

--- TR.caMonPageMod.JRCMon/Pages/SystemControl/MenuPage.cs
+++ TR.caMonPageMod.JRCMon/Pages/SystemControl/MenuPage.cs
@@ -1,5 +1,6 @@
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 using TR.caMonPageMod.JRCMon.Pages.Correction;
 using TR.caMonPageMod.JRCMon.Pages.Maintenance;
@@ -8,50 +9,76 @@ using TR.caMonPageMod.JRCMon.Pages.Other;
 namespace TR.caMonPageMod.JRCMon.Pages.SystemControl;
 
 [PageTypes.NormalPage("メニュー", ResourceManager.ResourceFiles.MenuIcon)]
-public partial class MenuPage : NormalPageBase, IHoldRootGridInstance
+public partial class MenuPage : Canvas, IHoldRootGridInstance
 {
 	public RootGrid? RootGrid { get; set; }
-
-	private readonly Button GoToSleepStateButton = ComponentFactory.GetEmptyButton(
-		new(633, 464, 0, 0),
-		104,
-		38
-	);
 
 	const int FIRST_COL_X = 90;
 	const int COL_STEP_X = 159;
 	const int FIRST_ROW_Y = 35;
 	const int ROW_STEP_Y = 144;
+	const int BUTTON_WIDTH = 113;
+	const int BUTTON_HEIGHT = 68;
 
-	public MenuPage() : base(ResourceManager.ResourceFiles.MenuPage)
+	public MenuPage()
 	{
-		AddMenuButton(3, 0);
+		AddButton(3, 0, ResourceManager.ResourceFiles.ToCIcon, "目次画面");
 
-		AddMenuButton(0, 1);
-		AddMenuButton(1, 1);
-		AddMenuButton<MaintenanceMenuPage>(2, 1);
-		AddMenuButton<EmbeddedManual>(3, 1);
+		AddButton(0, 1, ResourceManager.ResourceFiles.DriverIcon, "運転士");
+		AddButton(1, 1, ResourceManager.ResourceFiles.ConductorIcon, "車　掌");
+		AddButton<MaintenanceMenuPage>(2, 1, ResourceManager.ResourceFiles.MaintenanceIcon, "検　修");
+		AddButton<EmbeddedManual>(3, 1, ResourceManager.ResourceFiles.EmbeddedManualIcon, "応急ﾏﾆｭｱﾙ");
 
-		AddMenuButton(0, 2);
-		AddMenuButton(1, 2);
-		AddMenuButton<OccupancyRatePage>(2, 2);
-		AddMenuButton<CorrectionMenu>(3, 2);
+		AddButton(0, 2, ResourceManager.ResourceFiles.WorkSettingIcon, "運行設定");
+		AddButton(1, 2, ResourceManager.ResourceFiles.CarInfoIcon, "車両状態");
+		AddButton<OccupancyRatePage>(2, 2, ResourceManager.ResourceFiles.OccupancyRateIcon, "乗車率");
+		AddButton<CorrectionMenu>(3, 2, ResourceManager.ResourceFiles.CorrectionIcon, "補　正");
 
-		GoToSleepStateButton.Click += (s, e) => RootGrid?.SetPageType<SleepState>();
-		Children.Add(GoToSleepStateButton);
+		AddSleepButton();
 	}
 
-	void AddMenuButton<T>(int col, int row, bool isNotImplemented = false) where T: FrameworkElement, new()
+	void AddSleepButton()
 	{
-		int x = FIRST_COL_X + COL_STEP_X * col;
-		int y = FIRST_ROW_Y + ROW_STEP_Y * row;
-		Button btn = ComponentFactory.GetEmptyButton(new(x, y, 0, 0), 113, 68);
-		if (isNotImplemented)
-			btn.Content = "未実装";
-		else
-			btn.Click += (s, e) => RootGrid?.SetPageType<T>();
+		Button btn = ComponentFactory.GetBasicButton(
+			new(633, 464, 0, 0),
+			104,
+			38,
+			isSmall: true,
+			color: Colors.Yellow,
+			isShadowColored: true
+		);
+
+		Label label = ComponentFactory.Get1XLabel();
+		label.Content = "画面消去";
+		label.Foreground = Brushes.Black;
+		btn.Content = label;
+
+		btn.Click += (s, e) => RootGrid?.SetPageType<SleepState>();
 		Children.Add(btn);
 	}
-	void AddMenuButton(int col, int row)
-		=> AddMenuButton<FrameworkElement>(col, row, true);
+
+	void AddButton<T>(int col, int row, ResourceManager.ResourceFiles resource, string labelStr, bool isNotImplemented = false) where T : FrameworkElement, new()
+	{
+		Image img = ResourceManager.GetResourceAsImage(resource);
+		img.Stretch = Stretch.None;
+		int x = FIRST_COL_X + COL_STEP_X * col;
+		int y = FIRST_ROW_Y + ROW_STEP_Y * row;
+		Button btn = ComponentFactory.GetBasicButton(new(x, y, 0, 0), BUTTON_WIDTH, BUTTON_HEIGHT);
+		if (!isNotImplemented)
+		{
+			btn.Click += (s, e) => RootGrid?.SetPageType<T>();
+		}
+		btn.Content = img;
+		Children.Add(btn);
+
+		Label label = ComponentFactory.Get1XLong2Label();
+		label.Content = labelStr;
+		label.Margin = new(x, y + BUTTON_HEIGHT, 0, 0);
+		label.Padding = new(0, 4, 0, 0);
+		label.Width = BUTTON_WIDTH;
+		label.HorizontalContentAlignment = HorizontalAlignment.Center;
+		Children.Add(label);
+	}
+	void AddButton(int col, int row, ResourceManager.ResourceFiles resource, string labelStr)
+		=> AddButton<FrameworkElement>(col, row, resource, labelStr, true);
 }
