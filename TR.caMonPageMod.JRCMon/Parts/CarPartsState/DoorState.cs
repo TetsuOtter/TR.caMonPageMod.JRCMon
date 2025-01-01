34a25a95bc8981800ab27ec8b25c57a89bcccb78
TetsuOtter <31824852+TetsuOtter@users.noreply.github.com>
2025-01-01 15:49:00 +0900

    ドア状態表示コンポーネントを追加

---
+++ TR.caMonPageMod.JRCMon/Parts/CarPartsState/DoorState.cs
@@ -0,0 +1,47 @@
+using System.Windows.Media;
+
+using TR.BIDSSMemLib;
+
+namespace TR.caMonPageMod.JRCMon.Parts.CarPartsState;
+
+public class DoorState : CarPartsStateBase
+{
+	bool isEnabled = true;
+	bool isDoorClosed = true;
+	public DoorState(int carIndex, int top) : base(carIndex, top)
+	{
+		OnChangeDoorState(true, true);
+
+		SMemLib.SMC_BSMDChanged += (s, e) =>
+		{
+			if (e.NewValue.IsDoorClosed == isDoorClosed && isEnabled == e.NewValue.IsEnabled)
+				return;
+			try
+			{
+				Dispatcher.Invoke(() => OnChangeDoorState(e.NewValue.IsDoorClosed, e.NewValue.IsEnabled));
+			}
+			catch (Exception ex)
+			{
+				System.Diagnostics.Debug.WriteLine(ex);
+			}
+		};
+	}
+
+	void OnChangeDoorState(bool isDoorClosed, bool IsEnabled)
+	{
+		this.isDoorClosed = isDoorClosed;
+		this.isEnabled = IsEnabled;
+		if (IsEnabled)
+		{
+			Background = isDoorClosed ? Brushes.Transparent : Brushes.Red;
+			Foreground = isDoorClosed ? Brushes.White : Brushes.Black;
+			Text = isDoorClosed ? "閉" : "開";
+		}
+		else
+		{
+			Background = Brushes.Transparent;
+			Foreground = Brushes.White;
+			Text = "-";
+		}
+	}
+}
